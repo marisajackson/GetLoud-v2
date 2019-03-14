e8626dc8d41e7aa6f5d05b3d7f8ffc69c8f227d0
MaRisa Jackson <mhljackson@gmail.com>
2019-03-14 17:31:40 -0500

    Assets precompile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module GetLoudV2
     config.load_defaults 5.2
 
     config.active_job.queue_adapter = :sidekiq
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
