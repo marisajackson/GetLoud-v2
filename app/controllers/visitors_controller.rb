11f659e364734c74ae7830e1fe801b954b14acf6
MaRisa Jackson <mhljackson@gmail.com>
2019-03-15 12:57:56 -0500

    Edge cases

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,10 +3,14 @@ class VisitorsController < ApplicationController
       if user_signed_in?
         if current_user.metro_area
           spotify_user = SpotifyUser.includes(:artists => :events).where(artists: {events: {metro_area: current_user.metro_area}}).find_by(user_id: current_user.id)
-          @events = spotify_user.artists
-          # https://open.spotify.com/user/oreolistens/playlist/3tCx3pnNsPKT6PsFKWygKo?si=VeLQcvdzRmupI_lNW415iA
-          playlist = Playlist.find_by(spotify_user_id: spotify_user.id)
-          @playlist_url = "https://open.spotify.com/user/#{spotify_user.spotify_id}/playlist/#{playlist.spotify_id}"
+          @events = nil
+          @playlist_url = nil
+          if spotify_user && spotify_user.artists
+            @events = spotify_user.artists
+            # https://open.spotify.com/user/oreolistens/playlist/3tCx3pnNsPKT6PsFKWygKo?si=VeLQcvdzRmupI_lNW415iA
+            playlist = Playlist.find_by(spotify_user_id: spotify_user.id)
+            @playlist_url = "https://open.spotify.com/user/#{spotify_user.spotify_id}/playlist/#{playlist.spotify_id}"
+          end
           # .to_json(:include => :events)
           # render :json => @events
           render "users/home"
