77504c8c0899ce4819b9aa3cb104c04b898b44e1
MaRisa Jackson <mhljackson@gmail.com>
2019-03-15 13:30:42 -0500

    Less event imports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
       @user.metro_area = params[:metroArea]
       @user.save!
 
-      EventImportJob.perform_later @user.metro_area
+      event = Event.where('metro_area', params[:metroArea]).first
+
+      if(!event)
+        EventImportJob.perform_later @user.metro_area
+      end
 
       render :json => current_user['metro_area']
     end
