57c6973450b6a3df1babab6fa6b131563ce63b69
MaRisa Jackson <mhljackson@gmail.com>
2019-11-23 13:16:33 -0600

    Remove ticketsnow events (duplicates)

--- app/jobs/event_import_job.rb
+++ app/jobs/event_import_job.rb
@@ -5,6 +5,7 @@ class EventImportJob < ApplicationJob
     logger.info "Starting Job: Event Import for : #{metro_area}"
     ticketmaster_service = TicketmasterService.new
     ticketmaster_service.import_events(metro_area)
+    Event.where("ticket_url LIKE '%ticketsnow%'").destroy_all
     logger.info "Finishing Job: Event Import for : #{metro_area}"
   end
 end
